f71b5875cb9c9c2de243293f4e818a6e93072c8a
David Perez <davperez@cobogpgp1203.local>
2015-08-21 14:22:20 -0500

    List Modules

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,44 +1,32 @@
 (function(){
 	'use strict';
+
+	var api_key = 'api_key=c6445617d42ba37094ff06ac4923599a';
 	var Configuration = Backbone.Model.extend({
+		url: 'http://api.themoviedb.org/3/configuration?&api_key=c6445617d42ba37094ff06ac4923599a',
 		defaults : {
 			baseURL : '',
 			site : '',
 			filePath : ''
 		}
 	});
+
 	var Movie = Backbone.Model.extend({
 		defaults : {
 			title : '', // Movie's name
-			description : '', // Movie's description
-			rate : '', // Movie's rate
-			poster : '', // Movie's poster
-			wasSaw : '' // Was or not saw the movie
+			overview : '', // Movie's description
+			userRate : '', // Movie's rate
+			posterPath : '', // Movie's poster
+			wasSaw :  false// Was or not saw the movie
 		}
 	});
 
 	var ListMovies = Backbone.Collection.extend({
-		url_base : 'http://api.themoviedb.org/3/search/movie?api_key=c6445617d42ba37094ff06ac4923599a&?query=',
-		url : '',
-		model : Movie
-	});
-
-	var MovieViewResult = Backbone.View.extend({
-		tagName : 'li',
-		className : 'itemMovie',
-
-		events : {
-			'click .addMovie' : 'selectMovie',
-			'click .viewCard' : 'viewCard'
-		},
-
-		render : function (item){
-			$(this.el).append('<h1>'+this.model.get('title')+'</h1>');
+		url : 'http://api.themoviedb.org/3/search/movie',
+		model : Movie,
+		parse : function (response) {
+			return response.results;
 		}
-	});
-
-	var listMoviesView = Backbone.Collection.extend({
-
 	});
 
 	var SearchView = Backbone.View.extend({
@@ -54,27 +42,35 @@
 		},
 
 		render : function () {
-			$(this.el).append('<input id="search" placeholder="Search movie"/>');
+			$(this.el).append('<input id="inputSearch" placeholder="Search movie"/> <input id="search" type="submit"  value="Search"/>');
 			return this;
 		},
 
 		searchMovies : function (e) {
-			var value = $(e.currentTarget).val(),
-			url_base = 'http://api.themoviedb.org/3/search/movie?api_key=c6445617d42ba37094ff06ac4923599a&?query=';
-
 			var listMovies = new ListMovies();
-			listMovies.set({
-				url : url_base + this.value
-			});
-			console.log(listMovies);
+			listMovies.url += '?' + api_key + '&query=' + $(this.el).find('#inputSearch').val();
+
+
 			listMovies.fetch({
-				success : function (arrayData) {
-					arrayData.toJSON();
+				success : function (objectData) {
+					var results = objectData.toJSON();
+
+					_.each(results, function(val){
+
+						var movie = new Movie({
+							title : val.title,
+							overview : val.overview,
+							posterPath : val.poster_path
+						});
+
+						listMovies.add(movie);
+					});
+
 				}
 			})
 		}
 
-
 	});
+
 	var searchView = new SearchView();
 })();
