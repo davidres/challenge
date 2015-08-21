f52ec3df5ee07bf9717e6bac16c973dc8025cfe4
David Perez <davperez@cobogpgp1203.local>
2015-08-21 15:11:58 -0500

    render movies

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,5 @@
 (function(){
+
 	'use strict';
 
 	var api_key = 'api_key=c6445617d42ba37094ff06ac4923599a';
@@ -21,6 +22,21 @@
 		}
 	});
 
+	var MovieViewResult = Backbone.View.extend({
+		tagName : 'li',
+		className : 'itemMovie',
+
+		events : {
+			'click .addMovie' : 'selectMovie',
+			'click .viewCard' : 'viewCard'
+		},
+
+		render : function (item){
+			$(this.el).append('<h1>'+this.model.get('title')+'</h1>');
+			return this;
+		}
+	});
+
 	var ListMovies = Backbone.Collection.extend({
 		url : 'http://api.themoviedb.org/3/search/movie',
 		model : Movie,
@@ -37,37 +53,50 @@
 		},
 
 		initialize : function(){
-			var listMovies = new ListMovies();
+			this.listMovies = new ListMovies();
+			this.listMovies.bind('add', this.appendResult);
 			this.render();
 		},
 
 		render : function () {
 			$(this.el).append('<input id="inputSearch" placeholder="Search movie"/> <input id="search" type="submit"  value="Search"/>');
+			$(this.el).append('<ul></ul>');
 			return this;
 		},
 
 		searchMovies : function (e) {
-			var listMovies = new ListMovies();
-			listMovies.url += '?' + api_key + '&query=' + $(this.el).find('#inputSearch').val();
+			var that = this;
 
+			this.listMovies.url += '?' + api_key + '&query=' + $(this.el).find('#inputSearch').val();
 
-			listMovies.fetch({
+			this.listMovies.fetch({
 				success : function (objectData) {
-					var results = objectData.toJSON();
-
-					_.each(results, function(val){
-
-						var movie = new Movie({
-							title : val.title,
-							overview : val.overview,
-							posterPath : val.poster_path
-						});
-
-						listMovies.add(movie);
-					});
-
+					that.setMoviesList(objectData);
 				}
-			})
+			});
+		},
+
+		setMoviesList : function (objectData) {
+			var results = objectData.toJSON(),
+			that = this;
+
+			_.each(results, function(val){
+				var movie = new Movie({
+					title : val.title,
+					overview : val.overview,
+					posterPath : val.poster_path
+				});
+
+				that.listMovies.add(movie);
+			});
+			console.log(this.listMovies.toJSON());
+		},
+
+		appendResult : function (item) {
+			var itemMovie = new MovieViewResult({
+				model : item
+			});
+			$('ul',this.el).append(itemMovie.render().el)
 		}
 
 	});
