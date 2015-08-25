fb7ce1ca58890a909b9e965a3ad688c673a0ac75
David Perez <davperez@cobogpgp1203.local>
2015-08-25 14:50:29 -0500

    To arrange files to their corresponding folder

--- public/scripts/models/movie.js
+++ public/scripts/models/movie.js
@@ -1,1 +1,13 @@
-var Movie = Backbone.models.extend({});
+(function(APP){
+	'use strict';
+	
+	APP.Movie = Backbone.Model.extend({
+		defaults : {
+			title : '', // Movie's name
+			overview : '', // Movie's description
+			userRate : '', // Movie's rate
+			posterPath : '', // Movie's poster
+			isFavorite : false
+		}
+	});
+})(APP);
--- public/scripts/views/listOfSearchMovies.js
+++
@@ -1,1 +0,0 @@
-var listMoviesView = Backbone.View.extend({});
